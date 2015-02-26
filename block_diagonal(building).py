cf2ef289b61f9e4a86ff540de10efb6b582a3fdb
Anuj Menta <anujmenta@gmail.com>
2015-02-26 10:48:32 +0530

    Intuitive

--- block_diagonal(building).py
+++ block_diagonal(building).py
@@ -7,29 +7,24 @@ def TDMAsolver(a, b, c, d):
     '''
     
     n = len(b)
-    x = ['a', 'b','c', 'd']
-    a = (np.array(a)).astype(float)
-    b = (np.array(b)).astype(float)
-    c = (np.array(c)).astype(float)
-    d = (np.array(d)).astype(float)
     #Changed to floats
 
     aa, bb, cc, dd = [0]*n, [0]*n, [0]*n, [0]*n
 
     #initialized
 
-    cc[0] = c[0]/b[0]
-    dd[0] = d[0]/b[0]
+    cc[0] = (np.linalg.inv(b[0]))*c[0]
+    dd[0] = (np.linalg.inv(b[0]))*d[0]
 
     for j in range(1,n):
         bb[j] = b[j] - a[j]*cc[j-1]
-        cc[j] = c[j]/bb[j]
-        dd[j] = (d[j]-a[j]*dd[j-1])/bb[j]
+        cc[j] = (np.linalg.inv(bb[j]))*c[j]
+        dd[j] = (np.linalg.inv(bb[j]))*(d[j]-a[j]*dd[j-1])
+
+    print bb, cc, dd
 
     for num in range(n-2,-1,-1):
         dd[num] = dd[num] - cc[num]*dd[num+1]
     
-    for i in range(len(dd)):
-        dd[i] = round(dd[i],3)
 
     return dd
